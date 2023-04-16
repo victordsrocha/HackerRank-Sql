579c631f99c354052e563b518fbfad146bf6d6d3
Victor Rocha <43556498+victordsrocha@users.noreply.github.com>
2023-04-16 13:37:18 -0300

    Create Aggregation.sql

---
+++ Aggregation/Aggregation.sql
@@ -0,0 +1,6 @@
+-- https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem?isFullScreen=true
+-- MS SQL SERVER
+
+SELECT COUNT(*) 
+FROM CITY
+WHERE population > 100000;
