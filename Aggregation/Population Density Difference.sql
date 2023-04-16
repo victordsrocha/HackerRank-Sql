9787995dffa1cc42fd3b31f9b21dd917caa12cb1
Victor Rocha <43556498+victordsrocha@users.noreply.github.com>
2023-04-16 13:57:40 -0300

    Create Population Density Difference.sql

---
+++ Aggregation/Population Density Difference.sql
@@ -0,0 +1,5 @@
+-- https://www.hackerrank.com/challenges/population-density-difference/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+-- MS SQL SERVER
+
+select max(population) - min(population)
+from city;
